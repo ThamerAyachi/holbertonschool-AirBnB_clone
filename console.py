c88dce93869e6cc7dca63a488efc0b47211acaec
ThamerAyachi <thamerjak3@gmail.com>
2023-02-23 23:03:11 +0100

    trying

--- console.py
+++ console.py
@@ -3,6 +3,11 @@
 import cmd
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 import models
 
 
@@ -12,7 +17,12 @@ class HBNBCommand (cmd.Cmd):
     prompt = '(hbnb) '
     classes = {
         "BaseModel": BaseModel,
-        "User": User
+        "User": User,
+        "Amenity": Amenity,
+        "City": City,
+        "Place": Place,
+        "Review": Review,
+        "State": State
     }
 
     def do_quit(self, line):
